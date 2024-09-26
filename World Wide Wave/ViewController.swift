41cc91660989abf247bf6338a9b68160dcf53bfd
Alexandrol20220521 <turuwatari@gmail.com>
2024-09-26 20:30:30 +0900

    Initial Commit

---
+++ World Wide Wave/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  World Wide Wave
+//
+//  Created by Ryutarou Nakajima on 2024/09/26.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
