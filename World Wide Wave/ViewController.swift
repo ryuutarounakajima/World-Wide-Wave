caf7735c7d0b4cffe48ef860fadef4444bfdf0ea
Alexandrol20220521 <turuwatari@gmail.com>
2024-10-01 21:01:17 +0900

    launch animation designing lauched 2024/10/1

--- World Wide Wave/ViewController.swift
+++ World Wide Wave/ViewController.swift
@@ -8,10 +8,21 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    var gradientLayer: CAGradientLayer!
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        
+        gradientLayer = CAGradientLayer()
+        gradientLayer.frame = view.bounds
+        gradientLayer.colors = [
+            UIColor.red.cgColor,
+            UIColor.blue.cgColor
+        ]
+        gradientLayer.startPoint = CGPoint(x: 0, y: 0)
+        gradientLayer.endPoint = CGPoint(x: 1, y: 0)
     }
 
 
