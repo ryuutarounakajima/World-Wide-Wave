77702d30ea5937e62d4d70daee74c31aba1f572b
Alexandrol20220521 <turuwatari@gmail.com>
2024-10-04 15:46:18 +0900

    [ci skip] launch screen created 2024/10/4

--- World Wide Wave/ViewController.swift
+++ World Wide Wave/ViewController.swift
@@ -9,7 +9,20 @@ import UIKit
 
 class ViewController: UIViewController {
     
-    var gradientLayer: CAGradientLayer!
+    private let  imageView: UIImageView = {
+        
+        let imageView = UIImageView()
+        imageView.image = UIImage(named: "WaveLogLogo")
+        imageView.contentMode = .scaleAspectFill
+        imageView.translatesAutoresizingMaskIntoConstraints = false
+        
+        imageView.layer.cornerRadius = 100
+        imageView.clipsToBounds = true
+        
+        return imageView
+    }()
+    
+    private var gradientLayer: CAGradientLayer!
 
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -18,13 +31,49 @@ class ViewController: UIViewController {
         gradientLayer = CAGradientLayer()
         gradientLayer.frame = view.bounds
         gradientLayer.colors = [
-            UIColor.red.cgColor,
-            UIColor.blue.cgColor
+            UIColor.blue.cgColor,
+            UIColor.cyan.cgColor,
+            UIColor.white.cgColor,
+            UIColor.systemBrown.cgColor
         ]
-        gradientLayer.startPoint = CGPoint(x: 0, y: 0)
+        gradientLayer.startPoint = CGPoint(x: 0, y: 1)
         gradientLayer.endPoint = CGPoint(x: 1, y: 0)
+        
+        view.layer.addSublayer(gradientLayer)
+        view.addSubview(imageView)
+        
+        NSLayoutConstraint.activate([
+            imageView.centerXAnchor.constraint(equalTo: view.centerXAnchor),
+            imageView.centerYAnchor.constraint(equalTo: view.centerYAnchor),
+            imageView.widthAnchor.constraint(equalToConstant: 200),
+            imageView.heightAnchor.constraint(equalToConstant: 200)
+        ])
+        
+        animateZoom()
     }
-
+    
+    private func animateZoom() {
+        
+        imageView.transform = CGAffineTransform(scaleX: 0.1, y: 0.1)
+        
+        UIView.animate(withDuration: 2.0, delay: 0, options: [.curveEaseOut],
+                       animations: {
+            self.imageView.transform = CGAffineTransform(scaleX: 3.0, y: 3.0)
+            self.imageView.alpha = 0.0
+            
+        }, completion: nil)
+        
+        let fadeAnimation = CABasicAnimation(keyPath: "opacity")
+        fadeAnimation.fromValue = 1.0
+        fadeAnimation.toValue = 0.0
+        fadeAnimation.duration = 3.0
+        fadeAnimation.fillMode = .forwards
+        fadeAnimation.isRemovedOnCompletion = false
+        
+        gradientLayer.add(fadeAnimation, forKey: "fadeAnimation")
+        
+            
+    }
 
 }
 
