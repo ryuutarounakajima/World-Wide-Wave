29362602949e6282d4071826542927cd6b9fb56a
Alexandrol20220521 <turuwatari@gmail.com>
2024-11-25 14:20:28 +0900

    2024/11/25 Created map and show user current location.

--- World Wide Wave/Wave/WaveViewController.swift
+++ World Wide Wave/Wave/WaveViewController.swift
@@ -5,14 +5,81 @@
 //  Created by Ryutarou Nakajima on 2024/11/21.
 //
 
-import Foundation
+
 import UIKit
+import MapKit
 
 
-class WaveViewController: UIViewController {
+class WaveViewController: UIViewController, CLLocationManagerDelegate {
+    
+    var mapView: MKMapView!
+    var locationManager: CLLocationManager!
+    var shouldCenterOnUserLocation: Bool = true
     
     override func viewDidLoad() {
-        
         super.viewDidLoad()
+        
+        
+        mapView = MKMapView(frame: self.view.bounds)
+        mapView.autoresizingMask = [.flexibleWidth, .flexibleHeight]
+        self.view.addSubview(mapView)
+        
+        mapView.showsUserLocation = true
+        mapView.userTrackingMode = .follow
+        
+        locationManager = CLLocationManager()
+        locationManager.delegate = self
+        locationManager.desiredAccuracy = kCLLocationAccuracyHundredMeters
+        locationManager.requestWhenInUseAuthorization()
+        locationManager.startUpdatingLocation()
+        
+        currentPositon()
+        //tabBarSetting()
     }
+    
+
+    
+ 
+    
+    
+    func locationManager(_ manager: CLLocationManager, didFailWithError error: Error) {
+         
+        print("failed to get location: \(error.localizedDescription)")
+        
+        /*let fakeLocation = CLLocationCoordinate2D(latitude: 35.3225, longitude: 140.3654)
+        let region = MKCoordinateRegion(center: fakeLocation, span: MKCoordinateSpan(latitudeDelta: 0.01, longitudeDelta: 0.01))
+        
+        mapView.setRegion(region, animated: true)
+        
+        let annotaion = MKPointAnnotation()
+        annotaion.coordinate = fakeLocation
+        annotaion.title = "AQI"
+        annotaion.subtitle = "100"
+        mapView.addAnnotation(annotaion)
+        */
+    }
+        
+    
+    func currentPositon() {
+        
+        let resetButton = UIButton(frame: CGRect(x: self.view.bounds.width - 60, y: view.bounds.height - 110, width: 50, height: 50))
+        resetButton.backgroundColor = UIColor.orange.withAlphaComponent(0.5)
+        resetButton.layer.cornerRadius = 25
+        resetButton.clipsToBounds = true
+        resetButton.setImage(UIImage(systemName: "location.fill"), for: .normal)
+        resetButton.tintColor = UIColor.brown
+        resetButton.addTarget(self, action: #selector(resetToUserLocation), for: .touchUpInside)
+               
+                self.view.addSubview(resetButton)
+    }
+    
+    @objc func resetToUserLocation() {
+           shouldCenterOnUserLocation = true
+           if let location = locationManager.location {
+               let coordinate = location.coordinate
+               let region = MKCoordinateRegion(center: coordinate, span: MKCoordinateSpan(latitudeDelta: 0.01, longitudeDelta: 0.01))
+               mapView.setRegion(region, animated: true)
+           }
+       }
+    
 }
